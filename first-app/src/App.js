dc6dcc0d494899ccde0085d02801c0f8831bbac5
Michal Bokr <michal.bokr@ententee.com>
2022-07-12 15:09:33 +0200

    Todo list - first basic version

---
+++ first-app/src/App.js
@@ -0,0 +1,19 @@
+import Backdrop from "./components/Backdrop";
+import Todo from "./components/Todo";
+import Modal from "./components/Modal"
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text='Learn React'/>      
+      <Todo text='Master React'/>      
+      <Todo text='Explore full React course'/>      
+      <Modal />
+      <Backdrop />
+    </div>
+  );
+}
+
+
+export default App;
