5c88b5257d3b7471ab13d56cbaf00bb5ecb96f92
Michal Bokr <michal.bokr@ententee.com>
2022-07-13 12:55:18 +0200

    routing

--- meetup-app/src/App.js
+++ meetup-app/src/App.js
@@ -1,5 +1,25 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllMeetupsPage from "./pages/AllMeetups";
+import NewMeetup from "./pages/NewMeetup";
+import Favorites from "./pages/Favorites";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetup />
+        </Route>
+        <Route path="/favorites">
+          <Favorites />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
