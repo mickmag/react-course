81f4eacd81517b660e734e9fbb15203d80706060
Michal Bokr <michal.bokr@ententee.com>
2022-07-14 16:38:22 +0200

    post request

--- meetup-app/src/pages/NewMeetup.js
+++ meetup-app/src/pages/NewMeetup.js
@@ -1,5 +1,22 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetup() {
-    return <div>NewMeetup page</div>
+  function addMeetupHandler(meetupData) {
+    fetch("https://meetup-app-11edd-default-rtdb.firebaseio.com/meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetup;
