37e99c08f6965e609a02ad6d640bae459b0f7f40
Michal Bokr <michal.bokr@ententee.com>
2022-07-14 14:39:55 +0200

    props.children usage

--- meetup-app/src/App.js
+++ meetup-app/src/App.js
@@ -3,13 +3,12 @@ import {Route, Switch} from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetup from "./pages/NewMeetup";
 import Favorites from "./pages/Favorites";
-import MainNavigation from "./components/layout/MainNavigation";
+import Layout from "./components/layout/Layout";
 
 
 function App() {
     return (
-        <div>
-            <MainNavigation/>
+        <Layout>
             <Switch>
                 <Route path="/" exact>
                     <AllMeetupsPage/>
@@ -21,7 +20,7 @@ function App() {
                     <Favorites/>
                 </Route>
             </Switch>
-        </div>
+        </Layout>            
     );
 }
 
