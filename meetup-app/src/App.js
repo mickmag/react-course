639ad5d404309aa073b37607265d4da58a18f4f9
Michal Bokr <michal.bokr@ententee.com>
2022-07-14 14:19:44 +0200

    added navigation

--- meetup-app/src/App.js
+++ meetup-app/src/App.js
@@ -1,25 +1,28 @@
-import { Route, Switch } from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
 
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetup from "./pages/NewMeetup";
 import Favorites from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
+
 
 function App() {
-  return (
-    <div>
-      <Switch>
-        <Route path="/" exact>
-          <AllMeetupsPage />
-        </Route>
-        <Route path="/new-meetup">
-          <NewMeetup />
-        </Route>
-        <Route path="/favorites">
-          <Favorites />
-        </Route>
-      </Switch>
-    </div>
-  );
+    return (
+        <div>
+            <MainNavigation/>
+            <Switch>
+                <Route path="/" exact>
+                    <AllMeetupsPage/>
+                </Route>
+                <Route path="/new-meetup">
+                    <NewMeetup/>
+                </Route>
+                <Route path="/favorites">
+                    <Favorites/>
+                </Route>
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
